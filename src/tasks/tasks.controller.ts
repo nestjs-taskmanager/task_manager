e1d7c367dc370632c6748afd9ad1d4b93c176cca
odi <odi@panter.ch>
2024-03-28 12:21:36 +0100

    Remove unused import

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,7 +7,6 @@ import {
   Param,
   Delete,
   ParseIntPipe,
-  NotFoundException,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
