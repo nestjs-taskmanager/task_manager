4fda5090e87ebc7f2feba3eb68e8ee79167ce300
odi <odi@panter.ch>
2024-03-28 10:58:32 +0100

    Add validation pipe

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,22 +1,42 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty({
     description: 'The title of the task',
   })
   title: string;
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(240)
   @ApiProperty({
     description: 'The description of the task',
     required: false,
   })
   description?: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(1000)
   @ApiProperty({
     description: 'The body of the task',
   })
   body: string;
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({
     description: 'The completion status of the task',
     required: false,
